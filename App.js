bb8d2047261d16289cb6c167933a4778fa0f3992
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-07-28 15:04:49 -0300

    commit

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, ImageBackground, Text, View, TextInput, TouchableOpacity} from 'react-native';
+
+const image = { uri: "https://i.pinimg.com/originals/85/d6/b8/85d6b896ac59612102200dd3482009db.jpg" };
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground  style={{
+            position: "absolute",
+            height: '100%',
+            width: '100%'
+          }}source={image}>
+    </ImageBackground>
+      <View>
+        <Text style={styles.titulo}>Previsão do Tempo</Text>
+        </View>
+
+        <View>
+        <Text style={styles.label}>Cidade:</Text>
+        <TextInput
+        placeholder='Digite sua cidade'
+        style={styles.input}
+        />
+        </View>
+        <View>
+          <TouchableOpacity style={styles.botao}>
+            <Text style={styles.txtBotao}>Buscar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    top: 50,
+  },
+  titulo: {
+    fontSize:30,
+    textAlign:'center'
+  },
+  input:{
+    width:'80%',
+    marginLeft:'10%',
+    borderColor: 'purple',
+    borderBottomWidth:2,
+    marginTop: 20,
+    fontSize: 20,
+    top: 20,
+  },
+  
+  label:{
+    fontSize:20, 
+    top:35,
+  },
+  botao:{
+    width:'50%',
+    top: 50,
+    marginLeft: '25%',
+    fontSize: 30,
+    textAlign:'center',
+    height: 50,
+    backgroundColor: '#E6E6FA',
+    borderRadius: 50
+  },
+  txtBotao:{
+    fontSize:35,
+    textAlign:'center',
+    color: '#fff'
   },
 });
